0d6c912f5de6691e74e2fb6bba683e40434e5c68
Wissam Nusair <sam.nusair@gmail.com>
2021-02-03 15:47:58 +0000

    Initial commit

---
+++ js/questions.js
@@ -0,0 +1,135 @@
+let questions = [
+    {
+    numb: 1,
+    question: "La ropa",
+    answer: "clothes",
+    options: [
+      "clothes",
+      "vest",
+      "wristwatch",
+      "earrings"
+      ]
+    },
+    {
+    numb: 2,
+    question: "Una camiseta sin mangas",
+    answer: "Tank tops (Sleeveless shirt)",
+    options: [
+      "shirt (long sleeve)",
+      "Tank tops (Sleeveless shirt)",
+      "scarf",
+      "beanie"
+      ]
+    },
+    {
+      numb: 3,
+      question: "Una camiseta",
+      answer: "t-shirt",
+      options: [
+        "Clothes",
+        "Made of wool",
+        "t-shirt",
+        "Tie"
+      ]
+    },
+    {
+      numb: 4,
+      question: "Una camiseta (de manga larga)",
+      answer: "shirt (long sleeve)",
+      options: [
+        "jacket",
+        "jeans",
+        "jersey",
+        "shirt (long sleeve)"
+      ]
+    },
+    {
+      numb: 5,
+      question: "una sudadera",
+      answer: "sweatshirt",
+      options: [
+        "sweatshirt",
+        "zipper",
+        "belt",
+        "necklace"
+      ]
+    },
+    {
+      numb: 6,
+      question: "Is this a cool quiz (There is only one answer)",
+      answer: "Yes",
+      options: [
+        "Yes",
+        ".",
+        ".",
+        "."
+      ]
+    },
+    
+
+];
+
+/*
+
+la ropa	clothes
+una camiseta sin mangas	tank tops (sleeveless shirt)
+una camiseta	t-shirt
+una camisa (de manga larga)	shirt (long sleeve)
+una sudadera	sweatshirt
+un suéter, un jersey, un saco	sweater
+un suéter / una sudadera con capucha	hoodie
+un traje	suit
+una corbata	tie
+una blusa	blouse
+una falda	skirt
+un vestido	dress
+una chaqueta	jacket
+una bufanda	scarf
+un traje de baño	bathing/swimming suit
+unos pantalones	pants
+unos vaqueros, unos jeans	jeans
+una camisa (manga larga)	(long-sleeved) shirt
+un abrigo	coat
+unos guantes	gloves
+unos calcetines, unas medias	socks
+unas pijamas	pajamas
+un chaleco	vest
+unos pantalones cortos	shorts
+ropa interior: calzoncillos y pantaletas	underwear: briefs and panties
+unas medias largas	stockings, tights
+unos zapatos	shoes
+unos tacones (altos)	(high) heels
+unos zapatos deportivos, unos tenis	athletic shoes, sneakers
+unas botas	boots
+un impermeable	raincoat
+unas chanclas, unas chancletas	flip-flops
+unas sandalias	sandals
+una gorra	(baseball) cap
+un sombrero	hat
+un gorro (de lana)	woolen hat, beanie
+un cinturón	belt
+unos lentes, unas gafas	reading glasses
+unos aretes, unos zarcillos	earrings
+unos lentes de sol, unas gafas de sol	sunglasess
+un reloj	wristwatch
+un collar	necklace
+un brazalete, una manilla	bracelet, armband
+una cremallera, un cierre	zipper
+floreado	flowered, floral
+de cuadros	plaid, checked
+de rayas	striped
+de cuero	made of leather
+de lana	made of wool
+de moda, a la moda	fashionable, trendy
+Llevar puesto (a/os/as)	to wear, to have on
+Vestirse	to get dressed
+Quitarse	to take off
+Ponerse	to put on
+Probarse	to try on
+Abrocharse	to fasten, "to buckle-up/button up"
+Subirse la cremallera	to zip up
+Bajarse la cremallera	to unzip
+Atarse los zapatos	to tie one's shoes
+Desatarse los zapatos	to untie shoelaces
+
+*/
